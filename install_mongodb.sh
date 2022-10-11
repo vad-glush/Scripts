48f1c247787497f55e8c8188b7a25312b257e92e
Vadim Glushkov <vad-glush@yandex.ru>
2022-10-11 11:35:26 +0300

    update mongo sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,8 +3,7 @@
 #скачиваем и добавляем репозиторий
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-
-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 #обновляем индексы
 sudo apt-get update
